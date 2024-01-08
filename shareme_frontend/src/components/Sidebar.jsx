5c540f08616f6b63182ebe039c5ff2680f4cc7c5
Govind-Asawa <govindasawa23@gmail.com>
2024-01-08 16:32:21 -0600

    Created Sidebar with static categories

--- shareme_frontend/src/components/Sidebar.jsx
+++ shareme_frontend/src/components/Sidebar.jsx
@@ -1,3 +1,76 @@
-export default function Sidebar() {
-  return ;
+import React from 'react';
+import { NavLink, Link } from 'react-router-dom';
+import { RiHomeFill } from 'react-icons/ri';
+import { IoIosArrowForward } from 'react-icons/io';
+
+import logo from '../assets/logo.png';
+
+const isNotActiveStyle =
+  'flex items-center px-5 gap-3 text-gray-500 hover:text-black transition-all duration-100 ease-in-out capitalize';
+const isActiveStyle =
+  'flex items-center px-5 gap-3 font-extrabold border-r-2 border-black transition-all duration-100 ease-in-out capitalize';
+
+const categories = [
+  { name: 'Animals' },
+  { name: 'Cars' },
+  { name: 'Coding' },
+  { name: 'Kids' },
+  { name: 'Other' },
+];
+
+export default function Sidebar({ user, setToggleSidebar }) {
+  const handleLogoClick = () => {
+    if (setToggleSidebar) setToggleSidebar(false);
+  };
+  return (
+    <div className='flex flex-col justify-between h-full bg-white overflow-y-scroll min-w-210 hide-scrollbar'>
+      <div className='flex flex-col'>
+        <Link
+          to='/'
+          className='flex px-5 gap-2 my-6 pt-1 w-190 items-center'
+          onClick={handleLogoClick}
+        >
+          <img src={logo} alt='Logo' className='w-full' />
+        </Link>
+        <div className='flex flex-col gap-5'>
+          <NavLink
+            to='/'
+            className={({ isActive }) =>
+              isActive ? isActiveStyle : isNotActiveStyle
+            }
+            onClick={handleLogoClick}
+          >
+            <RiHomeFill />
+            Home
+          </NavLink>
+          <h3 className='mt-2 mx-5 text-base 2xl:text-xl'>
+            Discover Categories
+          </h3>
+          {categories.slice(0, -1).map((item) => {
+            return (
+            <NavLink
+              to={`/categories/${item.name}`}
+              className={({ isActive }) =>
+                isActive ? isActiveStyle : isNotActiveStyle
+            }
+            onClick={handleLogoClick}
+            key={item.name}
+            >
+              {item.name}
+            </NavLink>);
+          })}
+        </div>
+      </div>
+      {user && (
+        <Link 
+          to={`user-profile/${user._id}`}
+          className='flex items-center my-5 mb-3 mx-3 p-2 gap-2 bg-white shadow-lg'
+          onClick={handleLogoClick}
+        >
+          <img src={user.image} alt="user-profile" className='w-10 h-10 rounded-full'/>
+          <p>{user.userName} &gt; </p>
+        </Link>
+      )}
+    </div>
+  );
 }
--- shareme_frontend/src/container/Home.jsx
+++ shareme_frontend/src/container/Home.jsx
@@ -29,7 +29,7 @@ export default function Home() {
   return (
     <div className='flex bg-gray-50 md:flex-row flex-col h-screen transition-height duration-75 ease-out'>
       <div className='hidden md:flex h-screen flex-initial'>
-        <Sidebar user={user && user} closeToggle={setToggleSidebar} />
+        <Sidebar user={user && user}/>
       </div>
       <div className='flex md:hidden flex-row'>
         <div className='w-full flex justify-between items-center p-2 shadow-md'>
@@ -58,11 +58,10 @@ export default function Home() {
                 onClick={() => setToggleSidebar(false)}
               />
             </div>
-            <Sidebar user={user && user} closeToggle={setToggleSidebar} />
+            <Sidebar user={user && user} setToggleSidebar={setToggleSidebar} />
           </div>
         )}
       </div>
-
       <div className='pb-2 flex-1 h-screen overflow-y-scroll' ref={scrollRef}>
         <Routes>
           <Route path='/user-profile/:userId' element={<UserProfile />} />
