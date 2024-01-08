b83aaf25ba35745a5380f7719d4ff0c3aa846705
Govind-Asawa <govindasawa23@gmail.com>
2024-01-07 21:43:22 -0600

    Created responsive Navbar

--- shareme_frontend/src/container/Home.jsx
+++ shareme_frontend/src/container/Home.jsx
@@ -11,6 +11,7 @@ import logo from '../assets/logo.png';
 export default function Home() {
   const [user, setUser] = useState(undefined);
   const [toggleSidebar, setToggleSidebar] = useState(false);
+  const scrollRef = useRef(null);
 
   const userInfo =
     localStorage.getItem('user') !== undefined
@@ -21,40 +22,53 @@ export default function Home() {
     client.fetch(getUserQuery(userInfo.sub)).then((data) => setUser(data[0]));
   }, []);
 
+  useEffect(() => {
+    scrollRef.current.scrollTo(0, 0);
+  });
+
   return (
     <div className='flex bg-gray-50 md:flex-row flex-col h-screen transition-height duration-75 ease-out'>
       <div className='hidden md:flex h-screen flex-initial'>
-        <Sidebar />
+        <Sidebar user={user && user} closeToggle={setToggleSidebar} />
       </div>
-      <div className='flex md:hidden flex-row justify-between items-center p-2'>
-        <HiMenu
-          fontSize={40}
-          className='cursor-pointer'
-          onClick={() => setToggleSidebar(true)}
-        />
-        <Link to='/'>
-          <img src={logo} alt='logo' className='w-28' />
-        </Link>
-        <Link to={`user-profile/${user?._id}`}>
-          <img
-            src={user?.image}
-            alt='profile photo'
-            className='w-12 rounded-lg'
+      <div className='flex md:hidden flex-row'>
+        <div className='w-full flex justify-between items-center p-2 shadow-md'>
+          <HiMenu
+            fontSize={40}
+            className='cursor-pointer'
+            onClick={() => setToggleSidebar(true)}
           />
-        </Link>
-      </div>
-      {toggleSidebar && (
-        <div className='fixed w-4/5 bg-white h-screen overflow-y-auto z-10 animate-slide-in'>
-          <div className='absolute w-full flex justify-end items-center p-2'>
-            <AiFillCloseCircle
-              fontSize={30}
-              className='cursor-pointer'
-              onClick={() => setToggleSidebar(false)}
+          <Link to='/'>
+            <img src={logo} alt='logo' className='w-28' />
+          </Link>
+          <Link to={`user-profile/${user?._id}`}>
+            <img
+              src={user?.image}
+              alt='profile photo'
+              className='w-12 rounded-lg'
             />
-          </div>
-          <Sidebar />
+          </Link>
         </div>
-      )}
+        {toggleSidebar && (
+          <div className='fixed w-4/5 bg-white h-screen overflow-y-auto z-10 shadow-md animate-slide-in'>
+            <div className='absolute w-full flex justify-end items-center p-2'>
+              <AiFillCloseCircle
+                fontSize={30}
+                className='cursor-pointer'
+                onClick={() => setToggleSidebar(false)}
+              />
+            </div>
+            <Sidebar user={user && user} closeToggle={setToggleSidebar} />
+          </div>
+        )}
+      </div>
+
+      <div className='pb-2 flex-1 h-screen overflow-y-scroll' ref={scrollRef}>
+        <Routes>
+          <Route path='/user-profile/:userId' element={<UserProfile />} />
+          {/* Route for pins */}
+        </Routes>
+      </div>
     </div>
   );
 }
